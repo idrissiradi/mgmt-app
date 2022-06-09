bb52516bd56a5c5d5f65c57560fba7d2cd8bdd9e
idrissiradi <idrissiradi@gmail.com>
2022-06-09 19:49:18 +0100

    Frontend: Project queries & mutations

---
+++ client/src/mutations/projectMutations.js
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client';
+
+const ADD_PROJECT = gql`
+	mutation addProject(
+		$name: String!
+		$description: String!
+		$status: ProjectStatus!
+		$clientId: ID!
+	) {
+		addProject(
+			name: $name
+			description: $description
+			status: $status
+			clientId: $clientId
+		) {
+			id
+			name
+			status
+			description
+			client {
+				id
+				name
+				email
+				phone
+			}
+		}
+	}
+`;
+
+const DELETE_PROJECT = gql`
+	mutation deleteProject($id: ID!) {
+		deleteProject(id: $id) {
+			id
+		}
+	}
+`;
+
+export { ADD_PROJECT, DELETE_PROJECT };
