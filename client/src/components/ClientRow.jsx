7d9941990ac25357b95a22db8b3d99c65e659806
idrissiradi <idrissiradi@gmail.com>
2022-06-10 12:03:47 +0100

    Delete client with project

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -3,22 +3,23 @@ import { useMutation } from '@apollo/client';
 
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 function ClientRow({ client }) {
 	const [deleteClient] = useMutation(DELETE_CLIENT, {
 		variables: { id: client.id },
-		// refetchQueries: [{ query: GET_CLIENTS }],
-		update(cache, { data: { deleteClient } }) {
-			const { clients } = cache.readQuery({ query: GET_CLIENTS });
-			cache.writeQuery({
-				query: GET_CLIENTS,
-				data: {
-					clients: clients.filter(
-						(client) => client.id !== deleteClient.id,
-					),
-				},
-			});
-		},
+		refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+		// update(cache, { data: { deleteClient } }) {
+		// 	const { clients } = cache.readQuery({ query: GET_CLIENTS });
+		// 	cache.writeQuery({
+		// 		query: GET_CLIENTS,
+		// 		data: {
+		// 			clients: clients.filter(
+		// 				(client) => client.id !== deleteClient.id,
+		// 			),
+		// 		},
+		// 	});
+		// },
 	});
 
 	return (
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -5,6 +5,7 @@ import { GET_PROJECT } from '../queries/projectQueries';
 import Spinner from '../components/Spinner';
 import ClientInfo from '../components/ClientInfo';
 import DeleteProjectButton from '../components/DeleteProjectButton';
+import EditProjectForm from '../components/EditProjectForm';
 
 function Project() {
 	const { id } = useParams();
@@ -28,7 +29,10 @@ function Project() {
 					<p>{data.project.description}</p>
 					<h5 className='mt-3'>Project Status</h5>
 					<p className='lead'>{data.project.status}</p>
+
 					<ClientInfo client={data.project.client} />
+
+					<EditProjectForm project={data.project} />
 
 					<DeleteProjectButton projectId={data.project.id} />
 				</div>
